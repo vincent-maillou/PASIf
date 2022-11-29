997250998ecf42562d7144ac640af034a5c39e3c
Vincent Maillou <v.maillou@amolf.nl>
2022-11-29 09:50:53 +0100

    First steps
    Signed-off-by: Vincent Maillou <v.maillou@amolf.nl>

---
+++ source/GPUSolver.cu
@@ -0,0 +1,5 @@
+/**
+ * 
+*/
+
+#include "GPUSolver.cuh"
---
+++ source/PASIf.cpp
@@ -0,0 +1,8 @@
+/**
+ * 
+*/
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/chrono.h>
+namespace py = pybind11;
