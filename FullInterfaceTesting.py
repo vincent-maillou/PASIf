first gradient that compute something, but certainly wrong
Signed-off-by: vincent-maillou <vincent.maillou0@gmail.com>

FullInterfaceTesting.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 
 
 # Generate the excitation set
-excitationSet = []
-excitation : np.ndarray = np.ones(78001)
+excitation : np.ndarray = np.ones(2*78001)
 # Fill the excitation vector with a ramp
 """ excitation = []
 for i in range(78001):
       excitation.append(i/78001) """
-
-excitationSet.append(excitation)
-
+      
+n_excitations = 1
+excitationSet : list[np.ndarray] = [excitation] * n_excitations 
+      
 sampleRate = 16000
 
 displayCompute = True
@@ -29,31 +29,34 @@ pasif = PASIf(excitationSet, sampleRate, 0, displayCompute, displaySystem, displ
 
 n = 1
 
+systemSize = 8
+
 from scipy.sparse import dia_matrix
-M    : dia_matrix       = dia_matrix(([1., 1., 1., 1., 10., 1.], [0]), shape=(6, 6)) 
+M    : dia_matrix       = dia_matrix(([1., 1., 1., 1., 1., 10., 1., 1.], [0]), shape=(systemSize, systemSize)) 
 vecM : list[dia_matrix] = [M] * n
 
+
 from scipy.sparse import coo_matrix
-B    : coo_matrix       = coo_matrix(([-1, -1, -1, 1, 10], ([0, 1, 2, 3, 4], [3, 4, 5, 3, 4])), shape=(6, 6))
+B    : coo_matrix       = coo_matrix(([-1, -1, -1, -1, 1, 10], ([0, 1, 2, 3, 4, 5], [4, 5, 6, 7, 4, 5])), shape=(systemSize, systemSize))
 vecB : list[coo_matrix] = [B] * n
 
-K    : coo_matrix       = coo_matrix(([6, 10], ([3, 4], [0, 1])), shape=(6, 6))
+K    : coo_matrix       = coo_matrix(([6, 10], ([4, 5], [0, 1])), shape=(systemSize, systemSize))
 vecK : list[coo_matrix] = [K] * n
 
-Gamma : coo_tensor = coo_tensor(dimensions_ = [6, 6, 6])
-Gamma.val          = [-10, -10, -1]
-Gamma.indices      = [0,1,3 , 0,0,4 , 1,1,5]
+Gamma : coo_tensor = coo_tensor(dimensions_ = [systemSize, systemSize, systemSize])
+Gamma.val          = [-10, -10, -1, -1]
+Gamma.indices      = [0,1,4 , 0,0,5 , 0,0,6, 1,1,7]
 vecGamma : list[coo_tensor] = [Gamma] * n
 
-Lambda : coo_tensor = coo_tensor(dimensions_ = [6, 6, 6, 6])
+Lambda : coo_tensor = coo_tensor(dimensions_ = [systemSize, systemSize, systemSize, systemSize])
 Lambda.val          = [40000]
-Lambda.indices      = [1,1,1,4]
+Lambda.indices      = [1,1,1,5]
 vecLambda : list[coo_tensor] = [Lambda] * n
 
-forcePattern    : np.ndarray = np.array([0, 0, 0, 0.5, 0, 0])
+forcePattern    : np.ndarray = np.array([0, 0, 0, 0, 0.5, 0, 0, 0])
 vecForcePattern : list[np.ndarray] = [forcePattern] * n
 
-initialCondition    : np.ndarray = np.array([0, 0, 0, 0, 0, 0])
+initialCondition    : np.ndarray = np.zeros(systemSize)
 vecInitialCondition : list[np.ndarray] = [initialCondition] * n
 
 
@@ -93,23 +96,23 @@ modulationBuffer = np.array([1.0, 1.0, 1.0, 1.0])
 #pasif.setModulationBuffer(modulationBuffer)
 
 
-""" start   = time.time()
+start   = time.time()
 results = pasif.getAmplitudes()
 end     = time.time()
 
 print("setMatrix() + getAmplitude() overall time: ", end - start)
-print("Amplitudes: ", results) """
+print("Amplitudes: ", results)
 
 
 
 
-start      = time.time()
+""" start      = time.time()
 trajectory = pasif.getTrajectory(saveSteps = 1)
 end        = time.time()
 
 print("getTrajectories() overall time: ", end - start)
 plt.plot(trajectory[0], trajectory[1])
-plt.show()
+plt.show() """
 
 
 """ jac_M : dia_matrix = dia_matrix(([1., 1., 1., 1., 1., 1.], [0]), shape=(6, 6))
@@ -127,33 +130,88 @@ jac_Gamma.indices      = [0, 2, 0, 4, 1, 2, 5, 1, 0]
 vecJac_Gamma : list[coo_tensor] = [jac_Gamma] * n """
 
 
-M    : dia_matrix       = dia_matrix(([2., 2., 2., 2., 2., 2., 2., 2.], [0]), shape=(8, 8)) 
-vecM : list[dia_matrix] = [M] * n
+
 
 from scipy.sparse import coo_matrix
-B    : coo_matrix       = coo_matrix(([-5, -5, -5, -5, -5, -5, -5, -5], ([0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5, 6, 7])), shape=(8, 8))
+
+# From Theophile's example
+adjointSize = 12
+
+M_values  = np.ones(adjointSize)
+M    : dia_matrix       = dia_matrix((M_values, [0]), shape=(adjointSize, adjointSize)) 
+vecM : list[dia_matrix] = [M] * n
+
+
+B_dims    = [12, 12]
+B_values  = [1, 1, 1, 1]
+B_indexes = [4, 0, 5, 1, 6, 2, 7, 3]
+B_rows    = B_indexes[::2]
+B_cols    = B_indexes[1::2]
+
+B    : coo_matrix       = coo_matrix((B_values, (B_rows, B_cols)), shape=(B_dims[0], B_dims[1]))
 vecB : list[coo_matrix] = [B] * n
 
-K    : coo_matrix       = coo_matrix(([-6, -6, -6, -6, -6, -6, -6, -6], ([0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5, 6, 7])), shape=(8, 8))
+
+K_dims    = [12, 12]
+K_values  = [-12.0, -2.0, -0.5, -1.0]
+K_indexes = [0, 4, 4, 4, 1, 5, 5, 5]
+K_rows    = K_indexes[::2]
+K_cols    = K_indexes[1::2]
+K    : coo_matrix       = coo_matrix((K_values, (K_rows, K_cols)), shape=(K_dims[0], K_dims[1]))
 vecK : list[coo_matrix] = [K] * n
 
-Gamma : coo_tensor = coo_tensor(dimensions_ = [8, 8, 8])
-Gamma.val          = [-7, -7, -7]
-Gamma.indices      = [0,0,4 , 1,1,5 , 2,2,6]
+
+Gamma_dims     = [12, 12, 8]
+Gamma_values   = [-20.0, -20.0, -1.0, -1.0, -2.0, -0.1, -0.1, 1, 1]
+Gamma_indexes  = [0, 4, 1, 1, 4, 0, 8, 4, 0, 9, 4, 4, 0, 5, 0, 10, 5, 1, 11, 5, 5, 0, 6, 0, 1, 7, 1]
+
+Gamma_values   = [-2.0, 1, -20, -1.0, -20.0, 1, -0.1, -1.0, -0.1]
+Gamma_indexes  = [0, 5, 0,
+                  0, 6, 0,
+                  1, 4, 0,
+                  8, 4, 0,
+                  0, 4, 1,
+                  1, 7, 1,
+                  10, 5, 1,
+                  9, 4, 4,
+                  11, 5, 5]
+
+Gamma_dims     = [12, 12, 12]
+Gamma_values   = [-20.0, -20, -1.0, -1.0, -2.0, -0.1, -0.1, 1, 1]
+Gamma_indexes  = [0, 1, 4, 
+                  1, 0, 4, 
+                  8, 0, 4,
+                  9, 4, 4, 
+                  0, 0, 5,
+                  10, 1, 5, 
+                  11, 5, 5,
+                  0, 0, 6, 
+                  1, 1, 7]
+
+Gamma : coo_tensor = coo_tensor(dimensions_ = Gamma_dims)
+Gamma.val          = Gamma_values
+Gamma.indices      = Gamma_indexes
 vecGamma : list[coo_tensor] = [Gamma] * n
 
-Lambda : coo_tensor = coo_tensor(dimensions_ = [8, 8, 8, 8])
-Lambda.val          = [1]
-Lambda.indices      = [1,1,1,1]
+
+Lambda_dims    = [12, 12, 8, 8]
+Lambda_values  = [120000.0]
+Lambda_indexes = [1, 5, 1, 1]
+Lambda : coo_tensor = coo_tensor(dimensions_ = Lambda_dims)
+Lambda.val          = Lambda_values
+Lambda.indices      = Lambda_indexes
 vecLambda : list[coo_tensor] = [Lambda] * n
 
-forcePattern    : np.ndarray = np.array([0., 0., 0., 0., 0., 0., 0., 0.])
+forcePattern    : np.ndarray = np.zeros(adjointSize)
 vecForcePattern : list[np.ndarray] = [forcePattern] * n
 
-initialCondition    : np.ndarray = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+initialCondition    : np.ndarray = np.zeros(adjointSize)
+initialCondition[2] = 1.
+initialCondition[3] = 1.
 vecInitialCondition : list[np.ndarray] = [initialCondition] * n
 
-Psi : coo_tensor = coo_tensor(dimensions_ = [8, 8, 8, 8, 8])
+Psi_dims    = [12, 12, 12, 12, 12]
+Psi : coo_tensor = coo_tensor(dimensions_ = Psi_dims)
 Psi.val          = [1]
 Psi.indices      = [1,1,1,1,1]
 vecPsi : list[coo_tensor] = [Psi] * n
@@ -165,11 +223,12 @@ end        = time.time()
 
 print("setJacobian() overall time: ", end - start)
 
-""" start      = time.time()
+start      = time.time()
 gradient = pasif.getGradient()
 end        = time.time()
 
-print("getGradient() overall time: ", end - start) """
+print("getGradient() overall time: ", end - start)
+print("Gradient: ", gradient)
 
 """ plt.plot(gradient[0], gradient[1], 'r')
 plt.plot(gradient[0], gradient[2], 'b')

PASIf.py
@@ -415,22 +415,21 @@ class PASIf(__GpuDriver):
         
         gradient = self._getGradient(save_)
         
-        chunkSize    = 280
+        """ chunkSize    = 280
         numSetpoints = 279
         
         # re-arrange the computed trajectory in a plotable way.
         #numOfSavedSteps = int(self.numsteps*(self.interpolSize+1)/chunkSize)
         numOfSavedSteps = numSetpoints + chunkSize - 2 
-        totalSize = self.globalSystemSize + self.globalAdjointSize
-        unwrappedGradient = np.array([np.zeros(numOfSavedSteps) for i in range(totalSize + 1)])
-    
+        unwrappedGradient = np.array([np.zeros(numOfSavedSteps) for i in range(self.globalAdjointSize + 1)])
+        
         for t in range(numOfSavedSteps):
             # first row always contain time
             unwrappedGradient[0][t] = t*numSetpoints/(self.sampleRate*(self.interpolSize+1))
-            for i in range(totalSize):
-                unwrappedGradient[i+1][t] = gradient[t*totalSize + i]
+            for i in range(self.globalAdjointSize):
+                unwrappedGradient[i+1][t] = gradient[t*self.globalAdjointSize + i]
     
-        return unwrappedGradient
+        return unwrappedGradient """
         
         return gradient
 
@@ -458,8 +457,9 @@ class PASIf(__GpuDriver):
             assert inputVecM[i].shape[0] == inputVecM[i].shape[1], "The matrix M must be squared."
             assert inputVecB[i].shape[0] == inputVecB[i].shape[1], "The matrix B must be squared."
             assert inputVecK[i].shape[0] == inputVecK[i].shape[1], "The matrix K must be squared."
-            assert inputVecGamma[i].dimensions[0]  == inputVecGamma[i].dimensions[1]  == inputVecGamma[i].dimensions[2], "The Gamma tensor must be squared."
-            assert inputVecLambda[i].dimensions[0] == inputVecLambda[i].dimensions[1] == inputVecLambda[i].dimensions[2] == inputVecLambda[i].dimensions[3], "The Lambda tensor must be squared."
+            if inputVecPsi is None:
+                assert inputVecGamma[i].dimensions[0]  == inputVecGamma[i].dimensions[1]  == inputVecGamma[i].dimensions[2], "The System Gamma tensor must be squared."
+                assert inputVecLambda[i].dimensions[0] == inputVecLambda[i].dimensions[1] == inputVecLambda[i].dimensions[2] == inputVecLambda[i].dimensions[3], "The System Lambda tensor must be squared."
             
             assert inputVecB[i].shape[0]                 == ndofs, "The matrix B must have the same size as the matrix M."
             assert inputVecK[i].shape[0]                 == ndofs, "The matrix K must have the same size as the matrix M."
@@ -502,8 +502,11 @@ class PASIf(__GpuDriver):
             rowK  : np.ndarray = inputVecK[0].row
             colK  : np.ndarray = inputVecK[0].col
             
+            self.system_Gamma  = cp.deepcopy(inputVecGamma[0])
+            self.system_Lambda = cp.deepcopy(inputVecLambda[0])
+            
             for i in range(1, self.numberOfSystems):
-                diagM = np.concatenate((diagM, inputVecM[i].data)).flatten()
+                diagM = np.concatenate((diagM, inputVecM[i].data))
                 
                 valB = np.concatenate((valB, inputVecB[i].data))
                 rowB = np.concatenate((rowB, inputVecB[i].row + self.globalSystemSize))
@@ -513,13 +516,15 @@ class PASIf(__GpuDriver):
                 rowK = np.concatenate((rowK, inputVecK[i].row + self.globalSystemSize))
                 colK = np.concatenate((colK, inputVecK[i].col + self.globalSystemSize))
                 
-                inputVecGamma[0].concatenateTensor(inputVecGamma[i])
-                inputVecLambda[0].concatenateTensor(inputVecLambda[i])
+                self.system_Gamma.concatenateTensor(inputVecGamma[i])
+                self.system_Lambda.concatenateTensor(inputVecLambda[i])
                 inputVecForcePattern[0]      = np.concatenate((inputVecForcePattern[0], inputVecForcePattern[i]), axis = 0)
                 inputVecInitialConditions[0] = np.concatenate((inputVecInitialConditions[0], inputVecInitialConditions[i]), axis = 0)
                 
                 self.globalSystemSize += inputVecM[i].shape[0]
             
+
+            diagM         = diagM.reshape((self.globalSystemSize))
             self.system_M = dia_matrix((diagM, [0]),         shape = (self.globalSystemSize, self.globalSystemSize))
             self.system_B = coo_matrix((valB, (rowB, colB)), shape = (self.globalSystemSize, self.globalSystemSize))
             self.system_K = coo_matrix((valK, (rowK, colK)), shape = (self.globalSystemSize, self.globalSystemSize))
@@ -529,8 +534,9 @@ class PASIf(__GpuDriver):
             self.system_B = cp.deepcopy(inputVecB[0])
             self.system_K = cp.deepcopy(inputVecK[0])
             
-        self.system_Gamma             = cp.deepcopy(inputVecGamma[0])
-        self.system_Lambda            = cp.deepcopy(inputVecLambda[0])
+            self.system_Gamma  = cp.deepcopy(inputVecGamma[0])
+            self.system_Lambda = cp.deepcopy(inputVecLambda[0])
+            
         self.system_forcePattern      = cp.deepcopy(inputVecForcePattern[0])
         self.system_initialConditions = cp.deepcopy(inputVecInitialConditions[0])
         