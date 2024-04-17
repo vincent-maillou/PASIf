e16e91fecb8030af3986582c85bd8d3242f3e4af
Louvet <louvet@amolf.nl>
2024-04-17 16:42:46 +0200

    Updated test
    Signed-off-by: Louvet <louvet@amolf.nl>

--- examples/test_2DOFgammalambda.py
+++ examples/test_2DOFgammalambda.py
@@ -13,7 +13,7 @@ import time
 USE_SOUND_FILE=True
 TRAJ=True
 GRAD= False
-CHAIN=False
+CHAIN=True
 
 system = spr.MechanicalSystem()
 if USE_SOUND_FILE:
@@ -32,8 +32,8 @@ else:
     b_cant = 1000
     k_cant = 500
 
-gamma  = 1e4
-duffing  = 1e12 #lambda
+gamma  = 1e6
+duffing  = 1e10 #lambda
 
 force = 1
 filelength = 5000
@@ -54,35 +54,35 @@ dofName = 'cantilever'
 system.degreesOfFreedom[f'{dofName}'] = spr.ParametricVariable(m_cant)
 system.interactionPotentials[f'{dofName}_K'] =  spr.IntegerPotential(k_cant)
 system.interactionPotentials[f'{dofName}_K'].degreesOfFreedom[dofName] = 2
-system.interactionPotentials[f'{dofName}_K'].strength.parameterized = False
+system.interactionPotentials[f'{dofName}_K'].strength.parameterized = True
 system.interactionPotentials[f'{dofName}_B'] =  spr.LocalDamping(dofName, b_cant)
-system.interactionPotentials[f'{dofName}_B'].strength.parameterized = False
+system.interactionPotentials[f'{dofName}_B'].strength.parameterized = True
 
 # # # # spring
-# system.interactionPotentials[f'{dofName}_L'] =  spr.IntegerPotential(duffing)
-# system.interactionPotentials[f'{dofName}_L'].degreesOfFreedom[dofName] = 4
-# system.interactionPotentials[f'{dofName}_L'].strength.parameterized = True
+system.interactionPotentials[f'{dofName}_L'] =  spr.IntegerPotential(duffing)
+system.interactionPotentials[f'{dofName}_L'].degreesOfFreedom[dofName] = 4
+system.interactionPotentials[f'{dofName}_L'].strength.parameterized = True
 
 
 opticalDOF = 'oscillator'
 mechanicalDOF = 'cantilever'
 
 system.interactionPotentials[f'OptoCoup_{opticalDOF}_{mechanicalDOF}'] =  spr.IntegerPotential(-gamma)
-system.interactionPotentials[f'OptoCoup_{opticalDOF}_{mechanicalDOF}'].degreesOfFreedom[opticalDOF] = 1
+system.interactionPotentials[f'OptoCoup_{opticalDOF}_{mechanicalDOF}'].degreesOfFreedom[opticalDOF] = 2
 system.interactionPotentials[f'OptoCoup_{opticalDOF}_{mechanicalDOF}'].degreesOfFreedom[mechanicalDOF] = 1
 system.interactionPotentials[f'OptoCoup_{opticalDOF}_{mechanicalDOF}'].strength.parameterized = True
 
 
 if CHAIN:
     system = spr.MechanicalSystem()
-    n_sites=6
+    n_sites=7
     k_scale_local_relative=100
     k_scale = 10
-    gamma  = 1
-    duffing  = 10 #lambda
+    gamma  = .01
+    duffing  = 1 #lambda
     delay = .06
     delay_per_site = delay/n_sites
-    mass = k_scale * delay_per_site**2
+    mass = delay_per_site**2
     final_damping = k_scale * delay_per_site # equals sqrt(k_scale*m)
 
     for i in range(n_sites): # generate chain
@@ -104,23 +104,29 @@ if CHAIN:
             name_dof_2 = f'dof_{i+1}' if i!=(n_sites-2) else 'cantilever'
 
             system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'] =  spr.IntegerPotential(-gamma)
-            system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'].degreesOfFreedom[site_name] = 1
+            system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'].degreesOfFreedom[site_name] = 2
             system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'].degreesOfFreedom[name_dof_2] = 1
-            system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'].strength.parameterized = True
+
+
+            # system.interactionPotentials[f'OptoCoup_{site_name}_{name_dof_2}'].strength.parameterized = True
             # system.interactionPotentials[f'{site_name}_L'] =  spr.IntegerPotential(duffing)
             # system.interactionPotentials[f'{site_name}_L'].degreesOfFreedom[site_name] = 4
             # system.interactionPotentials[f'{site_name}_L'].strength.parameterized = True
         else: # terminal damping
             system.interactionPotentials[f'{site_name}_B'] = spr.LocalDamping(site_name, final_damping)
+            system.interactionPotentials[f'{site_name}_B'].strength.parameterized = True
     #optomechanical coupling (gamma)
 
 if not USE_SOUND_FILE:
-    system.excitationSources['step'] = spr.DirectCInjectionSource(force)
-    system.interactionPotentials[f'excitation'] = spr.Excitation(opticalDOF, 'step', 1e5)
+    system.excitationSources['step'] = spr.DirectCInjectionSource(f'{force}*(1-t/(NUMSTEPS*TIMESTEP))')
+    system.interactionPotentials[f'excitation'] = spr.Excitation(opticalDOF, 'step', 1e7)
     trainingSet = [0]
 
 else:
     trainingSet = ['/home/louvet/Documents/01_data/00_one_to_four/training/soundfile_2500']#, '/home/louvet/Documents/01_data/00_one_to_four/training/soundfile_2812', '/home/louvet/Documents/01_data/00_one_to_four/training/soundfile_2020']
+
+    # trainingSet = ['/home/louvet/Documents/01_data/00_one_to_four/training/soundfile_1010']
+    trainingSet = ['/home/louvet/Documents/01_data/00_one_to_four/training/soundfile_1012']
 
     sr = 16000*16
     excitationFileLength = 78001
@@ -129,36 +135,29 @@ else:
     system.excitationSources['soundData2'] = spr.BinaryFileSource(fileList=trainingSet,
                                                     fileLength=excitationFileLength,
                                                     fileDataType='double',
-                                                    #modulationFrequency=10502.564103,
                                                     log2Upsampling=2)
     system.interactionPotentials[f'excitation2'] = spr.Excitation(opticalDOF, 'soundData2', 1e3)
 
-# # Probe string
-# system.probes['system_output'] = spr.WindowedA2Probe(opticalDOF,
-#                                                      startIndex=0,
-#                                                      endIndex=numSteps)
+system.probes[f'probe__squared'] = spr.SimpleA2Probe(f'cantilever')
+system.probes[f'probe__multi'] = spr.SimpleA2MultiProbe(f'cantilever', f'oscillator')
 
-# # Define an adjoint source (used to compute the gradient efficiently: https://en.wikipedia.org/wiki/Adjoint_state_method)
-# system.interactionPotentials['adjoint_source'] = system.probes['system_output'].makeAdjointSource()
+# for i in range(1, 5):
+#     system.probes[f'probe__multi_{i}'] = spr.SimpleA2MultiProbe(f'dof_{i}', f'dof_{i+1}')
 
-# Probe cantilever
-system.probes['cant_output'] = spr.WindowedA2Probe(mechanicalDOF,
-                                                   startIndex=0,
-                                                   endIndex=numSteps)
-
-# Define an adjoint source (used to compute the gradient efficiently: https://en.wikipedia.org/wiki/Adjoint_state_method)
-system.interactionPotentials['adjoint_source'] = system.probes['cant_output'].makeAdjointSource()
 
 cppEnvironment = spr.CPPEnvironment(numSteps = int(numSteps),
                          timeStep = 1.0/sr,
                          numSweepSteps = len(trainingSet),
                          numThreads=1)
-start  = time.time()
-cppTrajectory = cppEnvironment.getTrajectories(system)
-print(f'CPP trajectory time is {time.time()-start}')
+if TRAJ:
+    start  = time.time()
+    cppTrajectory = cppEnvironment.getTrajectories(system)
+    print(f'CPP trajectory time is {time.time()-start}')
+
 start  = time.time()
 cppAmplitude = cppEnvironment.getAmplitudes(system, deleteTemp=False)
 print(f'CPP amplitude time is {time.time()-start}')
+
 if GRAD:
     start  = time.time()
     cppGrad = np.array(cppEnvironment.getGradients(system, deleteTemp=False))
@@ -179,7 +178,7 @@ cudaEnvironment = spr.CUDAEnvironment(vecSystem,
 if not USE_SOUND_FILE:
     excitation = []
     for i in range(filelength):
-        excitation.append(1*force)
+        excitation.append(force*(1-i/numSteps))
     excitationSet = [excitation]
     cudaEnvironment.setExcitations(excitationSet, timeStep = 1.0/sr)
  
@@ -190,7 +189,6 @@ start = time.time()
 amplitudes = cudaEnvironment.getAmplitudes(vecSystem)
 stop = time.time()
 print(f'Total getAmplitude() time: {stop-start} s')
-print("Probes amplitudes: ", amplitudes)
 
 if TRAJ:
     start = time.time()
@@ -204,14 +202,19 @@ if GRAD:
     stop = time.time()
     print(f'Total getGradients() time: {stop-start} s')
 
-cpp_stringCantileverProbeEnergy = round(cppTrajectory[-1, -1], 20)
-gpu_stringCantileverProbeEnergy = round(amplitudes[0][0], 20)
+cpp_probe = np.round(cppAmplitude[0], 20)
+gpu_probe = np.round(amplitudes, 20)
 
-print(cppAmplitude)
-print(amplitudes)
-print("CPU cantilever probe energy: ", cpp_stringCantileverProbeEnergy)
-print("GPU cantilever probe energy: ", gpu_stringCantileverProbeEnergy)
-print(f"Cantilever probe energy relative error: {100*np.abs(cpp_stringCantileverProbeEnergy-gpu_stringCantileverProbeEnergy)/cpp_stringCantileverProbeEnergy}")
+
+relative_eror_mean = 0
+for i in range(gpu_probe.shape[1]):
+    relative_error = 100*np.abs((cpp_probe[:,i]-gpu_probe[:,i])/cpp_probe[:,i])
+    # relative_error_spy = 100*np.abs((cpp_probe[:,i]-scipy_probe[i])/cpp_probe[:,i])
+
+    print(f"Probe {i}, gpu={gpu_probe[:,i]}, cpu={cpp_probe[:,i]}: {relative_error}")
+    relative_eror_mean += relative_error/gpu_probe.shape[1]
+
+print(f"Mean relative error: {relative_eror_mean}")
 
 if GRAD:
     i = 0
@@ -222,14 +225,52 @@ if GRAD:
 
 #   ----- Plotting -----   #
 
-if TRAJ:
-    if gpuTrajectory.shape[1]!=cppTrajectory.shape[0]:
-        new_gpu_traj = []
-        new_x = np.linspace(0, gpuTrajectory[0,-1], cppTrajectory.shape[0])
-        for i in range(1, gpuTrajectory.shape[0]):
-            new_gpu_traj.append(np.interp(new_x, gpuTrajectory[0,:], gpuTrajectory[i, :]))
+# x_cpu = cppTrajectory[:,0].copy()
+# y_cpu = cppTrajectory[:, n_sites-1].copy()
 
-        gpuTrajectory = np.array(new_gpu_traj)
+# delta_x = gpuTrajectory[1] - x_cpu
+# delta_y = gpuTrajectory[n_sites] - y_cpu
+
+# delta_x_y = np.trapz(delta_x*y_cpu, dx=1/sr)
+# x_delta_y = np.trapz(x_cpu*delta_y, dx=1/sr)
+# delta_x_delta_y = np.trapz(delta_x*delta_y, dx=1/sr)
+
+# print(f'delta_x_y is {100-np.abs((cpp_probe[:,1]-delta_x_y)/cpp_probe[:,1]*100)}% of error')
+# print(f'x_delta_y is {100-np.abs((cpp_probe[:,1]-x_delta_y)/cpp_probe[:,1]*100)}% of error')
+# print(f'delta_x_delta_y is {100-np.abs((cpp_probe[:,1]-delta_x_delta_y)/cpp_probe[:,1]*100)}% of error')
+
+if TRAJ:
+    for i in range(n_sites):
+        fig, axs = plt.subplots(2, constrained_layout=True)
+        fig.suptitle(f'DOF {i}', fontsize=16)
+        axs[0].set_title('CPP Trajectory')
+        axs[0].plot(gpuTrajectory[0], cppTrajectory[:,i])
+        axs[0].set_xlabel('time (s)')
+        axs[0].set_ylabel('amplitude')
+
+        axs[1].set_title('CUDA Trajectory')
+        axs[1].plot(gpuTrajectory[0], gpuTrajectory[i+1])
+        axs[1].set_xlabel('time (s)')
+        axs[1].set_ylabel('amplitude')
+
+
+    for i in range(gpu_probe.shape[1]):
+        fig, axs = plt.subplots(2, constrained_layout=True)
+        fig.suptitle(f'Probe {i}', fontsize=16)
+        axs[0].set_title('CPP Trajectory')
+        axs[0].plot(gpuTrajectory[0], cppTrajectory[:,n_sites*2+i])
+        axs[0].set_xlabel('time (s)')
+        axs[0].set_ylabel('amplitude')
+
+        axs[1].set_title('CUDA Trajectory')
+        axs[1].plot(gpuTrajectory[0], gpuTrajectory[i+1+2*n_sites+gpu_probe.shape[1]])
+        axs[1].set_xlabel('time (s)')
+        axs[1].set_ylabel('amplitude')
+
+
+    plt.show()
+
+    raise a
 
 
     fig, axs = plt.subplots(3, constrained_layout=True)
